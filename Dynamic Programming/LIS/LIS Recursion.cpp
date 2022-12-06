66eb9ab48bcf28ae486dbc424f1a1ee237bda583
Harsh Sharma <77538369+harshsH-28@users.noreply.github.com>
2022-12-06 20:23:12 +0530

    Create LIS Recursion.cpp

---
+++ Dynamic Programming/LIS/LIS Recursion.cpp
@@ -0,0 +1,19 @@
+    // Longest Increasing Subsequence By Recursion
+    
+    int findLIS(vector<int>& nums,int idx,int previdx,int n)
+    {
+        if(idx >= n)
+        return 0;
+        int ans1 = 0;
+        if(previdx == -1 || nums[idx] > nums[previdx])
+        {
+            ans1 = 1 + findLIS(nums, idx+1, idx,n);
+        }
+        int ans2 = findLIS(nums,idx+1,previdx,n);
+        return max(ans1,ans2);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        return findLIS(nums,0,-1,n);
+    }
