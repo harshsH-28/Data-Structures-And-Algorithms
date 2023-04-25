fe43f3b21a12e73ee373abbf382b84c1f9aa91c6
Harsh Sharma <77538369+harshsH-28@users.noreply.github.com>
2023-04-25 21:45:24 +0530

    Update Prim's Algorithm_adjList.cpp

--- Graphs/Prim's Algorithm/Prim's Algorithm_adjList.cpp
+++ Graphs/Prim's Algorithm/Prim's Algorithm_adjList.cpp
@@ -1,37 +1,38 @@
-    int primsAlgo(int V, vector<vector<int>> adj[])
+// Prim's Algorihtm using Adjacency List
+int primsAlgo(int V, vector<vector<int>> adj[])
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    vector<bool> visited(V, 0);
+
+    int src = 0;
+    pq.push({0, src});
+
+    int mstSum = 0;
+    while (!pq.empty())
     {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<bool> visited(V,0);
-      
-        int src = 0;
-        pq.push({0,src});
-        
-        int mstSum = 0;
-        while(!pq.empty())
+        pair<int, int> topEl = pq.top();
+        int vertex = topEl.second;
+        int weight = topEl.first;
+        pq.pop();
+
+        if (visited[vertex]) continue;
+
+        // Add the vertex to MST
+        visited[vertex] = 1;
+        mstSum += weight;
+
+        // Exploring the Adjacent Vertices
+        for (auto& adjNodes : adj[vertex])
         {
-            pair<int,int> topEl = pq.top();
-            int vertex = topEl.second;
-            int weight = topEl.first;
-            pq.pop();
-            
-            if(visited[vertex]) continue;
-            
-            // Add the vertex to MST
-            visited[vertex] = 1;
-            mstSum += weight;
-            
-            // Exploring the Adjacent Vertices
-            for(auto& adjNodes : adj[vertex])
+            int adjNode = adjNodes[0];
+            int edgeWt = adjNodes[1];
+
+            if (!visited[adjNode])
             {
-                int adjNode = adjNodes[0];
-                int edgeWt = adjNodes[1];
-                
-                if(!visited[adjNode])
-                {
-                    pq.push({edgeWt, adjNode});
-                }
+                pq.push({edgeWt, adjNode});
             }
         }
-        
-        return mstSum;
     }
+
+    return mstSum;
+}
